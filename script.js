cbc804624949530a43e30a180c7f5cbd7199d55e
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-05-08 09:19:52 +0200

    size adjusted

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import GameboardMaze from "./GameboardMaze.js";
 import Controls from "./Controls.js";
 
-const gameboard = new GameboardMaze(67, 87, 600, 600);
+const gameboard = new GameboardMaze(37, 87, 600, 600);
 const controls = new Controls(gameboard);
 
 // gameboard.createRandomMaze();
